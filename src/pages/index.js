a982ced06f64cfef5da186cb82dd7124e646dc37
huhn511 <sehe89@gmail.com>
2021-07-17 20:07:23 +0200

    add whitepaper

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -24,9 +24,26 @@ function HomepageHeader() {
     </header>
   );
 }
+function WhitePaperSection() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <a href="whitepaper.pdf"
+            target="_blank"
+            className="button button--secondary button--lg"
+          > Read our Whitepaper 📖</a>
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -35,6 +52,7 @@ export default function Home() {
       <main>
         <HomepageFeatures />
       </main>
+      <WhitePaperSection />
     </Layout>
   );
 }
