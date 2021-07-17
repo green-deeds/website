cbb796ad810b3494456390855bab06ea3f7f068d
r3p0x <svreiser@gmail.com>
2021-07-17 23:32:35 +0200

    adds basic colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,64 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import styles from './index.module.css'
+import HomepageFeatures from '../components/HomepageFeatures'
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/welcome">
+            className='button button--secondary button--lg'
+            to='/docs/welcome'
+          >
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
     </header>
-  );
+  )
 }
 function WhitePaperSection() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <div className={clsx('hero', styles.heroBanner)}>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <a href="whitepaper.pdf"
-            target="_blank"
-            className="button button--secondary button--lg"
-          > Read our Whitepaper 📖</a>
+          <a
+            href='whitepaper.pdf'
+            target='_blank'
+            className='button button--secondary button--lg'
+          >
+            {' '}
+            Read our Whitepaper 📖
+          </a>
         </div>
       </div>
-    </header>
-  );
+    </div>
+  )
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description='Description will go into a meta tag in <head />'
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
       <WhitePaperSection />
     </Layout>
-  );
+  )
 }
